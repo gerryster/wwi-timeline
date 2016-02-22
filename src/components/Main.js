de878e1c76039fe235236137c5ce665a02b87eb1
Ryan Gerry <gerryster@gmail.com>
2016-02-22 08:32:42 -0600

    Starting to customize things.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,16 @@
 require('normalize.css');
 require('styles/App.css');
 
-import React from 'react';
+import Timeline from './Timeline';
 
-let yeomanImage = require('../images/yeoman.png');
+import React from 'react';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <h1>WWI Interactive Timeline</h1>
+        <Timeline />
       </div>
     );
   }
---
+++ src/components/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Timeline extends React.Component {
+  render() {
+    return (
+      <div className="timeline">
+        <div className="notice">The timeline will go here!</div>
+      </div>
+    );
+  }
+}
+
+Timeline.defaultProps = {
+};
+
+export default Timeline;
