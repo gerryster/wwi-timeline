14fd64d1483815411b2eed3353fab6255b3e4e05
Ryan Gerry <gerryster@gmail.com>
2016-02-22 19:09:18 -0600

    Some math and a warning cleanup.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+const DAYS_IN_WAR = 1567;
+
 class Timeline extends React.Component {
   render() {
+    const widthCss = { width: `${DAYS_IN_WAR * 2}px;`};
     return (
-      <div className="timeline">
+      <div className="timeline" style={widthCss}>
         <div className="notice">The timeline will go here!</div>
       </div>
     );
