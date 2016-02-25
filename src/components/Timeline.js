19ed43333da64bd8ca0c793535fb0fa8b60f6984
Ryan Gerry <gerryster@gmail.com>
2016-02-25 08:25:45 -0600

    Some basic styling.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,13 +1,16 @@
+require('styles/Timeline.css');
+
 import React from 'react';
 
 const DAYS_IN_WAR = 1567;
+const DAY_WIDTH = 2;
 
 class Timeline extends React.Component {
   render() {
-    const widthCss = { width: `${DAYS_IN_WAR * 2}px;`};
+    const widthCss = { width: `${DAYS_IN_WAR * DAY_WIDTH}px`};
     return (
-      <div className="timeline" style={widthCss}>
-        <div className="notice">The timeline will go here!</div>
+      <div className="timeline">
+        <div className="notice" style={widthCss}>The timeline will go here!</div>
       </div>
     );
   }
