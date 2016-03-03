2f5d67b54be3442993bd0025b95cf63cdf18b34b
Ryan Gerry <gerryster@gmail.com>
2016-03-02 22:04:20 -0600

    Adding lodash and displaying the years.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,30 +1,35 @@
 require('styles/Timeline.css');
 
 //import moment from 'moment';
-var moment = require('moment');
+//var moment = require('moment');
 import React from 'react';
+import _ from 'lodash';
 
-const DAYS_IN_WAR = 1567;
 const DAY_WIDTH = 2;
-const WAR_START_DATE = new Date(1914, 6, 28);
-const YEARS_TO_DAYS = Object.freeze({1914: 57, 1915: 365, 1916: 366, 1917: 365, 1918: 315 });
+const DAYS_IN_WAR = 1567
+//const WAR_START_DATE = new Date(1914, 6, 28);
+const YEARS_TO_DAYS = Object.freeze({1914: 157, 1915: 365, 1916: 366, 1917: 365, 1918: 315 });
 
 class Timeline extends React.Component {
   render() {
-    console.log("where am i?");
-    console.log("Moment: ", moment().format('MMMM Do YYYY, h:mm:ss a'));
-    const widthCss = { width: `${DAYS_IN_WAR * DAY_WIDTH}px`};
+    const overallWidthCss = { width: `${(DAYS_IN_WAR * DAY_WIDTH) + 30}px`};
     return (
       <div className="timeline">
-        {this.renderYears()}
+        <div className="timeline-content" style={overallWidthCss}>
+          {this.renderYears()}
+        </div>
       </div>
     );
   }
 
   renderYears() {
-    const widthCss = { width: `${DAYS_IN_WAR * DAY_WIDTH}px`};
-    //<div className="y1914" style=""
-
+    return _.sortBy(_.keys(YEARS_TO_DAYS)).map((year) => {
+      const BORDER_WIDTH = 1;
+      const widthCss = { width: `${(YEARS_TO_DAYS[year] * DAY_WIDTH) - BORDER_WIDTH}px`};
+      return (<div className="year" style={widthCss} key={year}>
+        {year}
+      </div>);
+    });
   }
 
   isLeapYear(year) {
