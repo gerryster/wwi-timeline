abc423dd1708d698153a4bce55c2b31ad8e88660
Ryan Gerry <gerryster@gmail.com>
2016-03-27 08:16:29 -0500

    A start to months.

---
+++ src/components/Months.js
@@ -0,0 +1,13 @@
+require('styles/Timeline.css');
+
+import React from 'react';
+import _ from 'lodash';
+
+const Months = (props) => {
+
+};
+
+Timeline.defaultProps = {
+};
+
+export default Timeline;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 require('styles/Timeline.css');
 
+import Month from './Month.js'
 //import moment from 'moment';
 //var moment = require('moment');
 import React from 'react';
@@ -28,6 +29,7 @@ class Timeline extends React.Component {
       const widthCss = { width: `${(YEARS_TO_DAYS[year] * DAY_WIDTH) - BORDER_WIDTH}px`};
       return (<div className="year" style={widthCss} key={year}>
         {year}
+        <Months dayWidth={DAY_WIDTH} startDate={42} endDate={42}/>
       </div>);
     });
   }
