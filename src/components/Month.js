036bffb92972cc5b20f0bca9c6072b965c4fe60d
Ryan Gerry <gerryster@gmail.com>
2016-04-08 07:53:06 -0500

    A start to semi-months.
    I am going to change these so that the semi month divider is as close to
    the center as possible instead of at the beginning of the 16th day.

--- src/components/Month.js
+++ src/components/Month.js
@@ -4,6 +4,7 @@ import { BORDER_WIDTH as YEAR_BORDER_WIDTH } from './Year'
 import classNames from 'classnames';
 import React, { PropTypes } from 'react';
 
+const DAYS_REQUIRED_TO_DISPLAY_MONTH_LABEL = 9;
 const propTypes = {
   warMonth: PropTypes.object.isRequired,
   firstMonth: PropTypes.bool.isRequired,
@@ -21,10 +22,27 @@ const Month = ({ warMonth, firstMonth }) => {
 
   return (
     <div className={monthClasses} style={widthCss}>
-      {warMonth.numDays() > 8 && warMonth.startDate.format('MMM')}
+      <div className="label-header" style={widthCss}>
+        {warMonth.numDays() >= DAYS_REQUIRED_TO_DISPLAY_MONTH_LABEL && warMonth.startDate.format('MMM')}
+      </div>
+      <SemiMonth warMonth={warMonth} contianerStartWidth={contianerStartWidth}/>
     </div>
   )
 };
+
+const SEMI_MONTH_DIVIDER_WIDTH = 1;
+const SemiMonth = ({ warMonth, contianerStartWidth }) => {
+  if(warMonth.numDays() >= 15) { // works for WWI but not for a generic timeline
+    return (
+      <div className="semi-month">
+        <div className="first-half duration" style={{width: `${15 * DAY_WIDTH - contianerStartWidth}px`}}/>
+        <div className="second-half duration" style={{width: `${(warMonth.numDays() - 15) * DAY_WIDTH - SEMI_MONTH_DIVIDER_WIDTH}px`}}/>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
 
 Month.propTypes = propTypes;
 export default Month;
