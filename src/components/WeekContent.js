0283b155d335ab69c7bb40281e3cc82b13de3ab9
Ryan Gerry <gerryster@gmail.com>
2016-04-16 09:27:05 -0500

    Display the currently selected week in the content section!

--- src/components/WeekContent.js
+++ src/components/WeekContent.js
@@ -1,13 +1,31 @@
 require('styles/WeekContent.css');
 
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
-class WeekContent extends React.Component {
-  render() {
-    return (
-      <div className="week-content">week content</div>
-    );
+const propTypes = {
+  currentWeek: PropTypes.object,
+};
+
+const WeekContent = ({ currentWeek }) => {
+  let header = <h2>Select A Week</h2>;
+  if(currentWeek) {
+    header = <h2>{currentWeek.startDate.format('M/D/YYYY')} to {currentWeek.endDate.format('M/D/YYYY')}</h2>;
+  }
+
+  return (
+    <div className="week-content">
+      {header}
+    </div>
+  );
+}
+
+WeekContent.propTypes = propTypes;
+
+const mapStateToProps = (state) => {
+  return {
+    currentWeek: state.currentWeek,
   }
 }
 
-export default WeekContent;
+export default connect(mapStateToProps)(WeekContent);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,14 @@
 /* Combine all available reducers to a single root reducer. */
+import Duration from 'models/Duration';
+
+import moment from 'frozen-moment';
 import { combineReducers } from 'redux';
 
-const currentWeek = (state='initial', action) => {
-  //console.log("state: ", state, ", action: ", action);
+const currentWeek = (state=(new Duration(moment('1914-07-28'), moment('1914-08-01'))), action) => {
+  console.log("state: ", state, ", action: ", action);
   switch (action.type) {
     case 'SET_CURRENT_WEEK':
-      return action
+      return action.weekDuration
     default:
       return state;
   }
